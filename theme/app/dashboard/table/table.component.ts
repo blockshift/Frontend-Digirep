a5317e6892071baebdd0b98bd24de333f97d3458
DESKTOP-FQMDMVD\Junaid <junaid381.sj@gmail.com>
2017-11-23 12:04:40 +0500

    First Commit

---
+++ theme/app/dashboard/table/table.component.ts
@@ -0,0 +1,9 @@
+import { Component , trigger , state , style , transition , animate } from '@angular/core';
+
+@Component({
+    selector: 'table-cmp',
+    moduleId: module.id,
+    templateUrl: 'table.component.html'
+})
+
+export class TableComponent{}
---
+++ theme/app/sidebar/sidebar-routes.config.ts
@@ -0,0 +1,123 @@
+import {  RouteInfo } from './sidebar.metadata';
+import { TreeModule } from 'angular-tree-component';
+
+
+export const ROUTES = [
+    { path: 'dashboard', title: 'DashBoard',  icon: 'dashboard', class: ''  }, 
+    { path: 'user',  icon:'edit', title: 'Enroll', class: '' },
+    { path: 'table', title: 'Public Blockchain',  icon:'content_paste', class: '' } ,
+   //{ path: 'typography', title: 'Typography',  icon:'library_books', class: '' },
+  // { path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+    //{ path: 'maps', title: 'Maps',  icon:'location_on', class: '' },
+   { path: 'notifications', title: 'Notifications',  icon:'notifications', class: '' },
+  // { path: 'upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+];
+
+
+
+
+
+
+
+// export const PAGES_MENU = [
+//     {
+//       path: 'dashboard',
+//       children: [
+//         {
+//           path: 'dashboard',
+//           data: {
+//             menu: {
+//               title: 'general.menu.dashboard',
+//               icon: 'ion-android-home',
+//               selected: false,
+//               expanded: false,
+//               order: 0
+//             }
+//           }
+//         },
+//         {
+//           path: 'editors',
+//           data: {
+//             menu: {
+//               title: 'general.menu.editors',
+//               icon: 'ion-edit',
+//               selected: false,
+//               expanded: false,
+//               order: 100,
+//             }
+//           },
+//           children: [
+//             {
+//               path: 'ckeditor',
+//               data: {
+//                 menu: {
+//                   title: 'general.menu.ck_editor',
+//                 }
+//               }
+//             }
+//           ]
+//         },
+        
+       
+//         {
+//           path: 'formpage',  // path for our page
+//           data: { // custom menu declaration
+//             menu: {
+//               title: 'Enroll Users', // menu title
+//               icon: 'glyphicon glyphicon-education', 
+//               pathMatch: 'prefix', // use it if item children not displayed in menu
+//               selected: false,
+//               expanded: false,
+//               order: 0
+//             }
+//           }
+//         },
+  
+//          {
+//           path: 'enrolldegree',  // path for our page
+//           data: { // custom menu declaration
+//             menu: {
+//               title: 'Enroll Degree', // menu title
+//               icon: 'glyphicon glyphicon-education', // menu icon
+//               pathMatch: 'prefix', // use it if item children not displayed in menu
+//               selected: false,
+//               expanded: false,
+//               order: 0
+//             }
+//           }
+//         },
+        
+//         {
+//           path: '',
+//           data: {
+//             menu: {
+//               title: 'general.menu.pages',
+//               icon: 'ion-document',
+//               selected: false,
+//               expanded: false,
+//               order: 650,
+//             }
+//           },
+//           children: [
+//             {
+//               path: ['/login'],
+//               data: {
+//                 menu: {
+//                   title: 'general.menu.login'
+//                 }
+//               }
+//             },
+//             {
+//               path: ['/register'],
+//               data: {
+//                 menu: {
+//                   title: 'general.menu.register'
+//                 }
+//               }
+//             }
+//           ]
+//         },
+//             ]
+//     }
+//   ];
+  
